247445bcf7966fefc4e47fee97959691d4a58c04
matisin <matias.medina.s2@gmail.com>
2025-01-24 12:22:21 -0300

    addedd script to use rofi as power menu

--- bin/powermenu.sh
+++ bin/powermenu.sh
@@ -6,7 +6,8 @@ selected=$(echo -e "󰤄 Apagar\n󰑓 Reiniciar\n󰗽 Cerrar Sesión\n󰤄 Suspe
     -i \
     -p "Sistema " \
     -theme-str 'listview {columns: 1; lines: 5;}' \
-    -theme-str 'entry {placeholder: "Escoge una opción...";}'
+    -theme-str 'entry {placeholder: "Escoge una opción...";}' \
+    -theme-str 'window {height: 25%;}'
 )
 
 # Ejecutar comando según la selección
